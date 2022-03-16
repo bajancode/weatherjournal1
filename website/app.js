a91858d88ebfbdc03f5a62ce88b05deb00c40372
bajancode <victory.web.development@gmail.com>
2022-03-15 23:51:38 -0400

    first commit

---
+++ website/app.js
@@ -0,0 +1,6 @@
+/* Global Variables */
+const apiKey = "bb3530a526c054faae8a7e04f88b0fe0&units=imperial";
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
